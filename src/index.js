3827a921fe21e8f44e3b247a450f0c859ef6b7b3
vinjas <danielvinyastorres@gmail.com>
2021-04-19 14:44:45 +0200

    Setup basic React App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ".\index.css";
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+}
+
+
+
+ReactDOM.render(Counter, document.getElementById("root"))
